7b2ec2a449e8380a0983a6a474a262e86359dfec
djeetee@users.noreply.github.com <djeetee@users.noreply.github.com>
2018-04-11 10:14:00 -0400

    Added camera support

--- FlowerExpert/ViewController.swift
+++ FlowerExpert/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var pickedImageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true    // allow the user to edit the photo
+        imagePicker.sourceType = .camera
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // grab what the snapped pic
+        let pickedImage = info[UIImagePickerControllerOriginalImage]
+        
+        // load it int the image view
+        pickedImageView.image = pickedImage as? UIImage
+        
+        // bye bye picker
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func cameraButtonTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
