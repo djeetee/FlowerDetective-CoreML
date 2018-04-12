41ab8ad1d9fcc3695875361c053fb5657eccecb8
djeetee@users.noreply.github.com <djeetee@users.noreply.github.com>
2018-04-11 22:53:40 -0400

    jason api & decoding base code works

--- FlowerExpert/ViewController.swift
+++ FlowerExpert/ViewController.swift
@@ -11,6 +11,43 @@ import CoreML
 import Vision
 
 
+struct WikipediaData: Codable {
+    let batchComplete: String
+    let query: Query
+
+    enum CodingKeys : String, CodingKey {
+        case batchComplete = "batchcomplete"
+        case query
+    }
+}
+
+struct Query: Codable {
+    let pageIds: [String]
+    let pages: [String : Page]
+    
+    enum CodingKeys : String, CodingKey {
+        case pageIds = "pageids"
+        case pages
+    }
+}
+    
+struct Page: Codable {
+    let pageId: Int
+    let ns: Int
+    let title: String
+    let extract: String
+    
+    enum CodingKeys : String, CodingKey {
+        case pageId = "pageid"
+        case ns
+        case title
+        case extract
+    }
+}
+
+//WikipediaData(batchComplete: Optional(""), query: Optional(FlowerExpert.Query(pageIds: Optional(["1548538"]), pages: Optional(FlowerExpert.Page(pageId: nil, ns: nil, title: nil, extract: nil)))))
+
+
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var pickedImageView: UIImageView!
     
@@ -56,11 +93,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         // create a request and the completion handler
         let classRequest = VNCoreMLRequest(model: model) { (request, error) in
-            // Completion handler
+            // CoreML model completion handler
             let flowerClassification = request.results?.first as? VNClassificationObservation
             
+            let flowerName = (flowerClassification?.identifier.capitalized)!
+            
             // set the title bar to the classification result
-            self.navigationItem.title = flowerClassification?.identifier.capitalized
+            self.navigationItem.title = flowerName
+            
+            print(self.getWikipediaInfo(flowerName: flowerName))
         }
         
         // create the request handler
@@ -73,6 +114,60 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             print(error)
         }
         
+        
+    }
+    
+    func getWikipediaInfo(flowerName: String) -> String {
+        var flowerDescription = "N/A"
+        
+        let wikipediaURl = "https://en.wikipedia.org/w/api.php?"
+        let urlParameters = "format=json&" +
+                            "action=query&" +
+                            "prop=extracts&" +
+                            "exintro=&" +
+                            "explaintext=&" +
+                            "titles=" + flowerName.replacingOccurrences(of: " ", with: "%20") + "&" +
+                            "indexpageids=&" +
+                            "redirects=1"
+        
+  
+        guard let url = URL(string: wikipediaURl + urlParameters) else {
+            return flowerDescription
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            // network call completetion handler
+            if error != nil {
+                print("Response: \(error!)")
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let wikipediaData = try decoder.decode(WikipediaData.self, from: data)
+                
+                print(wikipediaData)
+                
+                let key = wikipediaData.query.pageIds.first!
+                print(key)
+                
+                let extract = wikipediaData.query.pages[key]?.extract
+                print(extract!)
+                
+                
+                //flowerDescription = wikipediaData
+            } catch {
+                print("Decoding: \(error)")
+            }
+        }
+            
+        task.resume()
+        
+        return flowerDescription
         
     }
     
